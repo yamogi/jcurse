45ce5e174e7dbd1de41157d782a7cae0d5812b51
Jens Melzer <melzer.jens@gmail.com>
2013-09-05 23:32:47 +0200

    Added mocks for unit testing.

---
+++ src/test/java/com/github/otakun/jcurse/AddonFileHandlerMock.java
@@ -0,0 +1,33 @@
+package com.github.otakun.jcurse;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AddonFileHandlerMock implements AddonFileHandler {
+
+	@Override
+	public void downloadToWow(Addon newAddon) {
+	}
+
+	@Override
+	public void downloadToWow(String downloadUrl, Addon addon) {
+	}
+
+	@Override
+	public void removeAddons(Collection<Addon> toDelete) {
+	}
+
+	@Override
+	public void removeAddonFolders(Collection<String> toDpublicelete) {
+	}
+
+	@Override
+	public String getCompressedFileName(String gameAddonNameId) {
+		return gameAddonNameId + "-1.0.zip";
+	}
+
+	@Override
+	public void downloadToWow(List<Addon> toDownload) {
+	}
+
+}
---
+++ src/test/java/com/github/otakun/jcurse/AddonRepoPersistenceMock.java
@@ -0,0 +1,29 @@
+package com.github.otakun.jcurse;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import com.github.otakun.jcurse.api.AddonRepoPersistence;
+
+public class AddonRepoPersistenceMock implements AddonRepoPersistence {
+
+	private final List<Addon> list = new ArrayList<>();
+	
+	public AddonRepoPersistenceMock() {
+		list.addAll(Addon.newInstance(Arrays.asList("test1", "test2")));
+	}
+	
+	@Override
+	public Collection<Addon> loadInstalledAddons() {
+		return list;
+	}
+
+	@Override
+	public void saveInstalledAddons(Collection<Addon> addons) {
+		list.clear();
+		list.addAll(addons);
+	}
+
+}
