f6e89584047a6b9e13e1ddcd260431f01e680788
Jens Melzer <melzer.jens@gmail.com>
2013-09-07 16:57:23 +0200

    Added testclass

---
+++ src/test/java/com/github/otakun/jcurse/CurseAddonFileHandlerTest.java
@@ -0,0 +1,14 @@
+package com.github.otakun.jcurse;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CurseAddonFileHandlerTest {
+
+	public void testDownloadToWoW() {
+		CurseAddonFileHandler fileHandler = new CurseAddonFileHandler();
+		List<Addon> addons = Addon.newInstance(Arrays.asList("test1", "test2"));
+		
+	}
+	
+}
