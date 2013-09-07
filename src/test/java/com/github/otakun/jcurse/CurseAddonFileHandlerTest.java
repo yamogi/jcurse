2fc1664885afa343cd0b78175cc3442ec10ab0b3
Jens Melzer <melzer.jens@gmail.com>
2013-09-07 22:11:08 +0200

    Test for removing folder.

--- src/test/java/com/github/otakun/jcurse/CurseAddonFileHandlerTest.java
+++ src/test/java/com/github/otakun/jcurse/CurseAddonFileHandlerTest.java
@@ -5,11 +5,14 @@ import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,8 +26,6 @@ public class CurseAddonFileHandlerTest {
 	private static final String URL_PLACEHOLDER = "#BASE_URL_TO_REPLACE#";
 	private static String baseUrl = null;
 
-	private File properties;
-	
 	@BeforeClass
 	public static void beforeClass() throws IOException {
 		baseUrl = Thread.currentThread().getContextClassLoader().
@@ -52,19 +53,47 @@ public class CurseAddonFileHandlerTest {
 		FileUtils.write(file, replaceOnce);
 	}
 
-	@Test
-	public void testDownloadToWoW() {
+	@Before
+	public void before() {
 		File root = folder.getRoot();
 		Configuration.getConfiguration().setWowFolder(root.getAbsolutePath());
+	}
+	
+	@Test
+	public void testDownloadToWoW() {
 		CurseAddonFileHandler fileHandler = new CurseAddonFileHandler();
 		List<Addon> addons = Addon.newInstance(Arrays.asList("bagnon"));
+		
 		fileHandler.downloadToWow(addons);
 
-		String rootPath = root.getAbsolutePath();
+		String rootPath = folder.getRoot().getAbsolutePath();
 		String addonPath = rootPath + File.separator + "Interface" + File.separator + "AddOns";
 		File addonRoot = new File(addonPath);
 		File[] listFiles = addonRoot.listFiles();
 		assertEquals(5, listFiles.length);
+	}
+	
+	@Test
+	public void testRemoveFolders() throws IOException {
+		File root = folder.getRoot();
+		String addonPath = root.getAbsolutePath() + File.separator + "Interface" + File.separator + "AddOns";
+		new File(addonPath + File.separator + "test1").mkdirs();
+		new File(addonPath + File.separator + "test2").mkdirs();
+		new File(addonPath + File.separator + "test3").mkdirs();
+
+		
+		List<Addon> addons = Addon.newInstance(Arrays.asList("test"));
+		Addon addon = addons.get(0);
+		Set<String> folders = new HashSet<>();
+		folders.add("test1");
+		folders.add("test2");
+		folders.add("test3");
+		addon.setFolders(folders);
+		
+		CurseAddonFileHandler fileHandler = new CurseAddonFileHandler();
+		fileHandler.removeAddons(addons);
+		
+		assertEquals(0, new File(addonPath).listFiles().length);
 	}
 
 }
