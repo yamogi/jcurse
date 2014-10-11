0b985585f4119a2427abedaf04e2892a5946ac3f
Jens Melzer <melzer.jens@gmail.com>
2014-10-11 19:29:30 +0200

    #30 Hopefully fixed indentation.

--- src/main/java/org/bitbucket/keiki/jcurse/data/ReleaseStatus.java
+++ src/main/java/org/bitbucket/keiki/jcurse/data/ReleaseStatus.java
@@ -1,37 +1,37 @@
 package org.bitbucket.keiki.jcurse.data;
 
 public enum ReleaseStatus {
-    ALPHA('a'), BETA('b'), RELEASE('r');
-    
-    private final char statusAbbr;
-    
-    private ReleaseStatus(char statusAbbreviation) {
-        this.statusAbbr = statusAbbreviation;
-    }
-    
-    public char getStatusAbbr() {
-        return statusAbbr;
-    }
+	ALPHA('a'), BETA('b'), RELEASE('r');
 
-    public static ReleaseStatus valueOf(char status) {
-        
-        switch(status) {
-        case 'r':
-            return RELEASE;
-        case 'b':
-            return BETA;
-        case 'a':
-            return ALPHA;
-        default:
-            throw new IllegalArgumentException(status + " is no valid status");
-        }
-    }
-    
-    public static ReleaseStatus valueOfIgnoreCase(String status) {
-    	try {
-    		return valueOf(status.toUpperCase());
-    	} catch (IllegalArgumentException e) {
-    		return null;
-    	}
-    }
+	private final char statusAbbr;
+
+	private ReleaseStatus(char statusAbbreviation) {
+		this.statusAbbr = statusAbbreviation;
+	}
+
+	public char getStatusAbbr() {
+		return statusAbbr;
+	}
+
+	public static ReleaseStatus valueOf(char status) {
+
+		switch (status) {
+		case 'r':
+			return RELEASE;
+		case 'b':
+			return BETA;
+		case 'a':
+			return ALPHA;
+		default:
+			throw new IllegalArgumentException(status + " is no valid status");
+		}
+	}
+
+	public static ReleaseStatus valueOfIgnoreCase(String status) {
+		try {
+			return valueOf(status.toUpperCase());
+		} catch (IllegalArgumentException e) {
+			return null;
+		}
+	}
 }
