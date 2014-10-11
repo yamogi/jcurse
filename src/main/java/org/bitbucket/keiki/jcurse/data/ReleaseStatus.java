bd0f90a8db78d98383a82149a35858bc3daec5da
Jens Melzer <melzer.jens@gmail.com>
2014-10-11 20:52:52 +0200

    #30 Fix code style again on ReleaseStatus.

--- src/main/java/org/bitbucket/keiki/jcurse/data/ReleaseStatus.java
+++ src/main/java/org/bitbucket/keiki/jcurse/data/ReleaseStatus.java
@@ -1,37 +1,37 @@
 package org.bitbucket.keiki.jcurse.data;
 
 public enum ReleaseStatus {
-	ALPHA('a'), BETA('b'), RELEASE('r');
+    ALPHA('a'), BETA('b'), RELEASE('r');
 
-	private final char statusAbbr;
+    private final char statusAbbr;
 
-	private ReleaseStatus(char statusAbbreviation) {
-		this.statusAbbr = statusAbbreviation;
-	}
+    private ReleaseStatus(char statusAbbreviation) {
+        this.statusAbbr = statusAbbreviation;
+    }
 
-	public char getStatusAbbr() {
-		return statusAbbr;
-	}
+    public char getStatusAbbr() {
+        return statusAbbr;
+    }
 
-	public static ReleaseStatus valueOf(char status) {
+    public static ReleaseStatus valueOf(char status) {
 
-		switch (status) {
-		case 'r':
-			return RELEASE;
-		case 'b':
-			return BETA;
-		case 'a':
-			return ALPHA;
-		default:
-			throw new IllegalArgumentException(status + " is no valid status");
-		}
-	}
+        switch (status) {
+        case 'r':
+            return RELEASE;
+        case 'b':
+            return BETA;
+        case 'a':
+            return ALPHA;
+        default:
+            throw new IllegalArgumentException(status + " is no valid status");
+        }
+    }
 
-	public static ReleaseStatus valueOfIgnoreCase(String status) {
-		try {
-			return valueOf(status.toUpperCase());
-		} catch (IllegalArgumentException e) {
-			return null;
-		}
-	}
+    public static ReleaseStatus valueOfIgnoreCase(String status) {
+        try {
+            return valueOf(status.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
 }
