208b0e16990ccf924620058dc8f977266a67d0ec
Jens Melzer <melzer.jens@gmail.com>
2014-10-25 13:33:09 +0200

    #30 Fixed line length

--- src/main/java/org/bitbucket/keiki/jcurse/io/Constants.java
+++ src/main/java/org/bitbucket/keiki/jcurse/io/Constants.java
@@ -1,7 +1,8 @@
 package org.bitbucket.keiki.jcurse.io;
 
 public final class Constants {
-    static final String USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/28.0.1500.71 Chrome/28.0.1500.71 Safari/537.36";
+    static final String USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)"
+            + " Ubuntu Chromium/28.0.1500.71 Chrome/28.0.1500.71 Safari/537.36";
     static final String CHARSET_WEBSITE = "UTF-8";
     
     private Constants() {
