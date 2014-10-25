6dca654c0cf2d1f8586f3e7005654ceb09c82ff7
Jens Melzer <melzer.jens@gmail.com>
2014-10-25 13:30:49 +0200

    #30 Extract magic number to constant.

--- src/main/java/org/bitbucket/keiki/jcurse/io/WebsiteHelper.java
+++ src/main/java/org/bitbucket/keiki/jcurse/io/WebsiteHelper.java
@@ -2,11 +2,13 @@ package org.bitbucket.keiki.jcurse.io;
 
 public class WebsiteHelper {
     
+    private static final int OFFSET_ATTRIBUTE_CONTENT_STARTS = 2;
+
     static String parseAttribute(String line, String attributeName) {
         String attribute = "";
         int indexOf = line.indexOf(attributeName);
         if (indexOf >= 0) {
-            int addonIdFrom = indexOf + attributeName.length() + 2;
+            int addonIdFrom = indexOf + attributeName.length() + OFFSET_ATTRIBUTE_CONTENT_STARTS;
             int indexOf2 = line.indexOf('\"', addonIdFrom);
             attribute = line.substring(addonIdFrom, indexOf2);
         }
